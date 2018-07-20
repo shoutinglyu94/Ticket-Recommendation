8c442797c2241460b57ccbcc61ccf2ab0f968f1b
Shouting Lyu <lyu.s@husky.neu.edu>
2018-07-19 22:32:14 -0400

    start a new project

---
+++ src/rpc/RecommendationItem.java
@@ -0,0 +1,63 @@
+package rpc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class RecommendationItem
+ */
+@WebServlet("/recommendation")
+public class RecommendationItem extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RecommendationItem() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		JSONArray array = new JSONArray();
+		try {
+			array.put(new JSONObject()
+					.put("username", "abcd")
+					.put("address", "san francisco")
+					.put("time", new Date().getTime()));
+			array.put(new JSONObject()
+					.put("username", "1234")
+					.put("address", "san jose")
+					.put("time", new Date().getTime()));
+		} catch(JSONException e) {
+			e.printStackTrace();
+		}
+		out.print(array);
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
